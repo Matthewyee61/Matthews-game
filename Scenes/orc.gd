054b33e442c492de9ec7ecc4b98ba4faf935a766
Matthewyee61 <matwat14@kingshigh.school.nz>
2024-08-28 14:58:48 +1200

    version 1.4
    Started making orc enemy for game

---
+++ Scenes/orc.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var direction = 1
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	translate(Vector2.RIGHT * direction)
+	$AnimatedSprite2D.flip_h = direction < 0
+
+
+
+func _on_timer_timeout() -> void:
+	direction *= -1
+	
+	
